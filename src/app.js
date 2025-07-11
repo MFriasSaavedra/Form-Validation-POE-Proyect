720a13e7928bf4f810d9c12dbe89ac1263c93389
MFriasSaavedra <martin.frias.saavedra@gmail.com>
2025-07-11 18:41:41 +0000

    solucion error navegador

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+window.onload = function () {
+  let form = document.getElementById("form");
+  let alerts = document.getElementById("alerts");
+  let cardValidation = document.getElementById("cardNumber");
+  let cvcValidation = document.getElementById("cvc");
+  let amountValidation = document.getElementById("amount");
+
+  function checkInfo() {
+    let hasError = false;
+
+    alerts.classList.add("d-none");
+
+    if (cardValidation.value === "") {
+      cardValidation.style.backgroundColor = "#eacbcaff";
+      cardValidation.style.borderColor = "red"
+      hasError = true;
+    }
+
+    if (cvcValidation.value === "") {
+      cvcValidation.style.backgroundColor = "#eacbcaff";
+      cvcValidation.style.borderColor = "red";
+      hasError = true;
+    }
+    if (amountValidation.value === "") {
+      amountValidation.style.backgroundColor = "#eacbcaff";
+      amountValidation.style.borderColor = "red";
+      hasError = true;
+    }
+    if (hasError) {
+      alerts.classList.remove("d-none");
+    }
+  }
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("hola")
+    checkInfo();
+  });
+}
